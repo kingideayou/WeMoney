709a1e52d67266ae86b88987bb9b3a026da1e1cd
NeXT <kingideayou@qq.com>
2017-08-23 15:21:04 +0800

    feature: 获取当前页面 ActivityName

--- app/src/main/java/me/next/wemoney/AccessibilityServiceTest.java
+++ app/src/main/java/me/next/wemoney/AccessibilityServiceTest.java
@@ -1,8 +1,8 @@
 package me.next.wemoney;
 
 import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import android.widget.Toast;
 
 /**
  * Created by NeXT on 17/8/8.
@@ -12,57 +12,27 @@ import android.widget.Toast;
 
 public class AccessibilityServiceTest extends AccessibilityService {
 
-    /*
-    @Override
-    protected void onServiceConnected() {
+    private static final String TAG = AccessibilityService.class.getSimpleName();
 
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        // Set the type of events that this service wants to listen to.  Others
-        // won't be passed to this service.
-        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED;
+    private static final String ACTIVITY_HOME = "LauncherUI";
+    private static final String ACTIVITY_CHAT_WITH = "En_5b8fbb1e"; //与他人聊天页，不常出现
+    private static final String ACTIVITY_PACKAGE = "En_fba4b94f"; //打开红包页
 
-        // If you only want this service to work with specific applications, set their
-        // package names here.  Otherwise, when the service is activated, it will listen
-        // to events from all applications.
-        info.packageNames = new String[]
-                {"com.tencent.mm"};
-
-        // Set the type of feedback your service will provide.
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_VISUAL;
-
-        // Default services are invoked only if no package-specific ones are present
-        // for the type of AccessibilityEvent generated.  This service *is*
-        // application-specific, so the flag isn't necessary.  If this was a
-        // general-purpose service, it would be worth considering setting the
-        // DEFAULT flag.
-
-        // info.flags = AccessibilityServiceInfo.DEFAULT;
-
-        info.notificationTimeout = 100;
-
-        this.setServiceInfo(info);
-
-    }
-    */
+    private String currentActivityName;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
-        final int eventType = accessibilityEvent.getEventType();
-        String eventText = null;
-        switch(eventType) {
-            case AccessibilityEvent.TYPE_VIEW_CLICKED:
-                eventText = "Clicked: ";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-                eventText = "Focused: ";
-                break;
+
+        getActivityName(accessibilityEvent);
+
+    }
+
+    private void getActivityName(AccessibilityEvent accessibilityEvent) {
+        if (accessibilityEvent.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            return;
         }
-
-        eventText = eventText + accessibilityEvent.getText();
-
-        // Do something nifty with this text, like speak the composed string
-        // back to the user.
-        Toast.makeText(getApplicationContext(), eventText, Toast.LENGTH_SHORT).show();
+        currentActivityName = accessibilityEvent.getClassName().toString();
+        Log.d(TAG, "currentActivityName : " + currentActivityName);
     }
 
     @Override
